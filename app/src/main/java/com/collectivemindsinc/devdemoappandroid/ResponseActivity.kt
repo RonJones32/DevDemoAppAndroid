0cb54d3e1111e2a332e61109f8ce98473a57dea0
SimonAJScott <simon.aj.scott@gmail.com>
2025-08-16 14:45:28 -0700

    just finished

--- app/src/main/java/com/collectivemindsinc/devdemoappandroid/ResponseActivity.kt
+++ app/src/main/java/com/collectivemindsinc/devdemoappandroid/ResponseActivity.kt
@@ -9,19 +9,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : ComponentActivity() {
+class ResponseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_response)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button = findViewById<Button>(R.id.findSimon)
+        val button = findViewById<Button>(R.id.findRon)
         button.setOnClickListener {
-            val intent = Intent(this, ResponseActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
